e2d3ebca114deb69969d607624c1f382b7522a96
Adetayo Timilehin <timilehin1999@gmail.com>
2022-11-15 15:47:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-/*
-const bitcoin = require('bitcoinjs-lib');
-const network = bitcoin.networks.testnet;
-
-const keypair = bitcoin.ECPair.makeRandom({ network });
-
-const pubkey = keypair.publicKey;
-
-const addressObject = bitcoin.payments.p2pkh({ pubkey, network });
-
-console.log(addressObject.address);
-*/
-
-
 // Create and send a Bitcoin transaction:
 //
 // 1. Query for unspent outputs.
 // 2. Create a transaction.
 // 3. Forward the transaction to the Bitcoin network.
 //
-// More information:
-// Example 3
-// https://www.mobilefish.com/developer/nodejs/nodejs_quickguide_bitcoinjs.html
-//
-// Disclaimer:
 //
 // Use this script at your own risk! All information on this page is provided "as is", without any warranty.
 // Mobilefish.com will not be liable for any damages, loss of profits or any other kind of loss
@@ -39,23 +20,21 @@ var satoshiToBTC = function(value) {
 
 // Broadcasts a transaction to the network via blockchain.info
 var broadcast_tx = function(tx) {
-
-	console.log("tx in hex = ", tx.toHex());
-
-	var options = {
+  console.log("tx in hex = ", tx.toHex());
+  var options = {
 		uri: 'https://blockchain.info/pushtx',
 		method: 'POST',
 		json: {
 			"tx": tx.toHex()
 		}
-	};
+  	     };
 
-	request(options, function(err, httpResponse, body) {
-		if (err) {
-			console.error('Request failed:', err);
-		} else {
-			console.log('Broadcast results:', body);
-			console.log("Transaction send with hash:", tx.getId());
+ request(options, function(err, httpResponse, body) {
+	if (err) {
+		console.error('Request failed:', err);
+	} else {
+		console.log('Broadcast results:', body);
+		console.log("Transaction send with hash:", tx.getId());
 		}
 	});
 }
@@ -79,49 +58,48 @@ prompt('Enter the private key of the source address (WIF format): ', function (p
 
 	request(url, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-
-			// Parse the response and get the first unspent output
-			var json = JSON.parse(body);
+		//Parse the response and get the first unspent output
+		var json = JSON.parse(body);
 			
-			var unspent = json["unspent_outputs"][0];
-			var unspentvalue = 4000
+		var unspent = json["unspent_outputs"][0];
+		var unspentvalue = 4000
 
-			console.log("JSON unspent", unspent);
+		console.log("JSON unspent", unspent);
 
-			// Prompt for the destination address
-			console.log("Found an unspent transaction output with ", satoshiToBTC(unspentvalue), " BTC.");
+		// Prompt for the destination address
+		console.log("Found an unspent transaction output with ", satoshiToBTC(unspentvalue), " BTC.");
 
-			prompt('Enter a destination address: ', function(dest_address) {
-				// Calculate the withdraw amount minus the tx fee
-				var withdraw_amount = unspent.value - tx_fee;
+		prompt('Enter a destination address: ', function(dest_address) {
+		// Calculate the withdraw amount minus the tx fee
+		var withdraw_amount = unspent.value - tx_fee;
 
-				console.log("Unspent value (BTC)= ", satoshiToBTC(unspentvalue));
-				console.log("Tx fee (BTC)= ", satoshiToBTC(tx_fee));
-				console.log("Withdraw amount (BTC)= ", satoshiToBTC(withdraw_amount));
+		console.log("Unspent value (BTC)= ", satoshiToBTC(unspentvalue));
+		console.log("Tx fee (BTC)= ", satoshiToBTC(tx_fee));
+		console.log("Withdraw amount (BTC)= ", satoshiToBTC(withdraw_amount));
 
-				// Build a transaction
-				console.log("TransactionBuilder input tx_hash_big_endian = ", unspent.tx_hash_big_endian);
-				console.log("TransactionBuilder input tx_output_n = ", unspent.tx_output_n);
+		// Build a transaction
+		console.log("TransactionBuilder input tx_hash_big_endian = ", unspent.tx_hash_big_endian);
+		console.log("TransactionBuilder input tx_output_n = ", unspent.tx_output_n);
                 
-				var returnAmount = 2589
+		var returnAmount = 2589
 
-				var txb = new bitcoin.TransactionBuilder();
-				txb.addInput(unspent.tx_hash_big_endian, unspent.tx_output_n);
-				txb.addOutput(dest_address, withdraw_amount);
-				txb.addOutput(source_address,  returnAmount)
+		var txb = new bitcoin.TransactionBuilder();
+		txb.addInput(unspent.tx_hash_big_endian, unspent.tx_output_n);
+		txb.addOutput(dest_address, withdraw_amount);
+		txb.addOutput(source_address,  returnAmount)
 
-				txb.sign(0, keyPair);
-				var tx = txb.build();
+		txb.sign(0, keyPair);
+		var tx = txb.build();
 
-				console.log("tx = ", tx);
+		console.log("tx = ", tx);
 
-				// Prompt to confirm sending the transaction
-				var confirm = "Send " + satoshiToBTC(withdraw_amount) + " plus miner fee? (y/N):";
-				prompt(confirm, function(result) {
-					if (result.toUpperCase() == "Y") {
-						broadcast_tx(tx);
-					};
-				});
+		// Prompt to confirm sending the transaction
+		var confirm = "Send " + satoshiToBTC(withdraw_amount) + " plus miner fee? (y/N):";
+		prompt(confirm, function(result) {
+		if (result.toUpperCase() == "Y") {
+			broadcast_tx(tx);
+		  .
+		});
 
 			});
 		} else {
